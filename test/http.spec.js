fc7447c0fcff5b28baca76c23578fc4e5242137f
Bowen <mr_lbw@outlook.com>
2018-07-26 12:17:14 +0800

    test(test): adjust test for marking sure app.genPromise has been resolved before testing

--- test/http.spec.js
+++ test/http.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const App = require('../dist/index')
 
-describe('Build Server', () => {
+describe('TEST: Application', () => {
   let app
   before(done => {
     app = new App()
@@ -12,16 +12,6 @@ describe('Build Server', () => {
   after(done => {
     Promise.resolve(app.server.close())
       .then(() => done())
-  })
-
-  it('GET: Home', done => {
-    request(app.server)
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        done()
-      })
   })
 
   it('Handle url error', done => {
@@ -47,15 +37,39 @@ describe('Build Server', () => {
       })
   })
 
-  // Make sure this function invoked after generator complete mission,
-  // otherwise gen.contentList is empty storage
-  it('GET: doc/sample', done => {
+  it('GET: Home', done => {
     request(app.server)
-      .get('/doc/sample')
+      .get('/')
       .expect(200)
       .end((err, res) => {
         if (err) throw err
         done()
       })
+  })
+
+  // Make sure this function invoked after generator complete mission,
+  // otherwise gen.contentList is empty storage
+  it('GET: doc/sample', done => {
+    // app.genPromise must be resolved before testing
+    app.genPromise.then(() => {
+      request(app.server)
+      .get('/doc/sample')
+      .expect(200, {
+        errno: 0,
+        to: "doc/sample",
+        title: "Hello world",
+        author: "Bowen",
+        date: "2018 AUG 01",
+        tags: [
+        "Javascript",
+        "Typescript"
+        ],
+        data: "\r\nThis is content of posts.\r\n"
+      })
+      .end((err, res) => {
+        if (err) throw err
+        done()
+      })
+    })
   })
 })
