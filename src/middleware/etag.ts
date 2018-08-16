5d3a0276fb159498333e50c5bf15bf11363bd8c6
Bowen <mr_lbw@outlook.com>
2018-08-16 16:28:47 +0800

    fix(Etag): correct Etag to string type rather than number type

--- src/middleware/etag.ts
+++ src/middleware/etag.ts
@@ -1,6 +1,8 @@
 import Koa = require('koa')
 import { Server } from '../server'
 
+const { stringify } = require('../utils/index')
+
 module.exports = function (etag: string) {
   return async function (ctx: Koa.Context, next: Function) {
     await next()
@@ -11,13 +13,16 @@ module.exports = function (etag: string) {
 }
 
 module.exports.createEtag = function createEtag () {
-  return Math.random().toString(16).slice(2)
+  return stringify(Math.random().toString(16).slice(2))
 }
 
 module.exports.identify = function createIdentify (__etag: Server['__etag']) {
   return async function (ctx: Koa.Context, next: Function) {
     if (ctx.request.header['if-none-match'] === __etag) {
       ctx.status = 304
+      ctx.set({
+        'Etag': __etag
+      })
       return
     }
     await next()
